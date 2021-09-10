4a13fba2e095e4ec61c5d8c2d90186f5beb5567d
vijayskk <vijayskk007@gmail.com>
2021-09-10 11:56:21 +0530

    about ready

--- src/script.js
+++ src/script.js
@@ -17,11 +17,14 @@ loader.load( '/textures/frog.glb', function ( gltf ) {
 
 } );
 
+const profilepic = new THREE.TextureLoader().load('/textures/pp.jpeg')
+
 
 const textureLoader = new THREE.TextureLoader()
 
-const normalTexture = textureLoader.load('/textures/NormalMap.png')
 const normalTexture2 = textureLoader.load('/textures/texture2.png')
+
+
 
 
 // Debug
@@ -36,9 +39,16 @@ const scene = new THREE.Scene()
 // Objects
 
 const geometry = new THREE.SphereGeometry( 1,42, 46 );
+const imagecube = new THREE.BoxGeometry( 2, 2, 2 );
 
 // Materials
  
+const imagecubematerial = new THREE.MeshStandardMaterial()
+imagecubematerial.color = new THREE.Color(0x424242)   
+imagecubematerial.roughness = 0.9
+imagecubematerial.metalness = 0.8
+imagecubematerial.map = profilepic
+
 const material = new THREE.MeshStandardMaterial()
 material.color = new THREE.Color(0x000000)   
 material.roughness = 0.3
@@ -46,27 +56,33 @@ material.wireframe = !true
 material.normalMap = normalTexture2
 // Mesh
 const sphere = new THREE.Mesh(geometry,material)
+const cube = new THREE.Mesh(imagecube,imagecubematerial)
+scene.add(cube)
 scene.add(sphere)
-
+cube.position.set(0,0,-5)
 // Lights
 
-const pointLight = new THREE.PointLight(0xffffff, 1)
+const pointLight = new THREE.PointLight(0xffffff, 6)
 pointLight.position.x = 4
 pointLight.position.y = 7
 pointLight.position.z = 2
 scene.add(pointLight)
 
 
-const pointLight2 = new THREE.PointLight(0xff0000, 4)
+const pointLight2 = new THREE.PointLight(0xff0000, 7)
 pointLight2.position.set(4,-1,1)
 scene.add(pointLight2)
 
-const pointLight3 = new THREE.PointLight(0x0000ff, 4)
+const pointLight3 = new THREE.PointLight(0x0000ff, 7)
 pointLight3.position.set(-4,-1,1)
 scene.add(pointLight3)
-const pointLight4 = new THREE.PointLight(0x00ff00, 4)
+const pointLight4 = new THREE.PointLight(0x00ff00, 7)
 pointLight4.position.set(0,1,1)
 scene.add(pointLight4)
+
+const cubelight = new THREE.PointLight(0xffffff, .01)
+cubelight.position.set(0,0,-5)
+scene.add(cubelight)
 
 // const whitelight = gui.addFolder('White light')
 
@@ -141,8 +157,8 @@ scene.add(pointLight4)
 // const lighthelper2 = new THREE.PointLightHelper(pointLight2 , .1)
 // scene.add(lighthelper2)
 
-// const lighthelper = new THREE.PointLightHelper(pointLight , .1)
-// scene.add(lighthelper)
+const cubelighthelper = new THREE.PointLightHelper(cubelight , .2)
+scene.add(cubelighthelper)
 
 /**
  * Sizes
@@ -226,11 +242,16 @@ const tick = () =>
 
     // Update objects
     sphere.rotation.y = .5 * elapsedTime
-    // sphere.rotation.x = .5 * elapsedTime
+    cube.rotation.y = .5 * elapsedTime
+    //sphere.rotation.x = .5 * elapsedTime
 
     sphere.rotation.y += .5 * (targetX - sphere.rotation.y)
     sphere.rotation.x += .5 * (targetY - sphere.rotation.x)
     sphere.rotation.z += -.5 * (targetY - sphere.rotation.x)
+
+    cube.rotation.y += .5 * (targetX - cube.rotation.y)
+    cube.rotation.x += .5 * (targetY - cube.rotation.x)
+    cube.rotation.z += -.5 * (targetY - cube.rotation.x)
     // Update Orbital Controls
     // controls.update()
 
